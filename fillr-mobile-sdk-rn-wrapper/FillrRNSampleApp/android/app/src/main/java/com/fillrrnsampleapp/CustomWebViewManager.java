d4abc815ece65b8c35e67b5b7ed491b0c66f43fd
Naveed Zanoon <naveed@fillr.com>
2019-10-04 15:32:51 +1000

    React Native Sample Integration (#10)
    * sample react native integration
    
    * specify keys
    
    * update sample integration

---
+++ fillr-mobile-sdk-rn-wrapper/FillrRNSampleApp/android/app/src/main/java/com/fillrrnsampleapp/CustomWebViewManager.java
@@ -0,0 +1,101 @@
+package com.fillrrnsampleapp;
+
+import android.support.annotation.Nullable;
+import android.webkit.WebResourceRequest;
+import android.webkit.WebResourceResponse;
+import android.webkit.WebView;
+
+import com.facebook.react.bridge.Arguments;
+import com.facebook.react.bridge.WritableMap;
+import com.facebook.react.common.MapBuilder;
+import com.facebook.react.module.annotations.ReactModule;
+import com.facebook.react.uimanager.ThemedReactContext;
+import com.facebook.react.uimanager.annotations.ReactProp;
+import com.facebook.react.views.webview.ReactWebViewManager;
+import com.fillr.browsersdk.Fillr;
+
+
+import java.util.Map;
+
+@ReactModule(name = CustomWebViewManager.REACT_CLASS)
+public class CustomWebViewManager extends ReactWebViewManager {
+    /* This name must match what we're referring to in JS */
+    protected static final String REACT_CLASS = "RCTCustomWebView";
+
+
+    protected static class CustomWebViewClient extends ReactWebViewClient {
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+            boolean shouldOverride = super.shouldOverrideUrlLoading(view, url);
+            String finalUrl = ((CustomWebView) view).getFinalUrl();
+
+            if (!shouldOverride && url != null && url.equals(finalUrl)) {
+                final WritableMap params = Arguments.createMap();
+                dispatchEvent(view, new NavigationCompletedEvent(view.getId(), params));
+            }
+            return shouldOverride;
+        }
+
+        @Override
+        public WebResourceResponse shouldInterceptRequest(WebView view, WebResourceRequest request) {
+            WebResourceResponse response = Fillr.getInstance().shouldInterceptRequest(view, request);
+            return response != null ? response : super.shouldInterceptRequest(view, request);
+        }
+
+        @Override
+        public void onPageFinished(WebView webView, String url) {
+            super.onPageFinished(webView, url);
+            Fillr instance = Fillr.getInstance();
+            instance.onPageFinished(webView);
+        }
+    }
+
+    protected static class CustomWebView extends ReactWebView {
+        public CustomWebView(ThemedReactContext reactContext) {
+            super(reactContext);
+        }
+
+        protected @Nullable String mFinalUrl;
+
+        public void setFinalUrl(String url) {
+            mFinalUrl = url;
+        }
+
+        public String getFinalUrl() {
+            return mFinalUrl;
+        }
+    }
+
+    @ReactProp(name = "finalUrl")
+    public void setFinalUrl(WebView view, String url) {
+        ((CustomWebView) view).setFinalUrl(url);
+    }
+
+    @Override
+    protected ReactWebView createReactWebViewInstance(ThemedReactContext reactContext) {
+        CustomWebView customWebView = new CustomWebView(reactContext);
+        Fillr.getInstance().trackWebView(customWebView);
+        return customWebView;
+    }
+
+    @Override
+    public String getName() {
+        return REACT_CLASS;
+    }
+
+    @Override
+    protected void addEventEmitters(ThemedReactContext reactContext, WebView view) {
+        view.setWebViewClient(new CustomWebViewClient());
+    }
+
+    @Override
+    public @Nullable
+    Map getExportedCustomDirectEventTypeConstants() {
+        Map<String, Object> export = super.getExportedCustomDirectEventTypeConstants();
+        if (export == null) {
+            export = MapBuilder.newHashMap();
+        }
+        export.put("navigationCompleted", MapBuilder.of("registrationName", "onNavigationCompleted"));
+        return export;
+    }
+}
