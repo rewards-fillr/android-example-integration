85424bce6bbbd7440a3e85dfad3f11795134808d
Naveed Zanoon <301696+hades-6@users.noreply.github.com>
2021-02-11 22:40:18 +1100

    Update react native integration (#21)
    * update project to reflect new version of react native
    
    * add webview
    
    * update integration
    
    Co-authored-by: Naveed Zanoon <@>

---
+++ fillr-mobile-sdk-rn-wrapper-v2/FillrRNSampleApp/App.js
@@ -0,0 +1,23 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React, { Component } from 'react';
+import { WebView } from 'react-native-webview';
+
+class MyWeb extends Component {
+  render() {
+    return (
+      <WebView
+        source={{ uri: 'https://www.fillr.com/test' }}
+        style={{ marginTop: 20 }}
+      />
+    );
+  }
+}
+
+export default MyWeb;
